a1413b584e0b74cf0c46ba639e3100cec8418a91
Aiura <104730381+Aiuraa@users.noreply.github.com>
2022-05-25 17:27:05 +0000

    Create samp-installer.sh
    Change wine path if udpated
    And changge the WINEPREFIX too if you install it on different location.
    Also, use this: https://lutris.net/games/grand-theft-auto-san-andreas/

---
+++ samp/samp-installer.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+WINEPREFIX=$HOME/Games/san-andreas-multiplayer WINEARCH=win32 $HOME/.local/share/lutris/runners/wine/lutris-fshack-7.2-x86_64/bin/wine $HOME/Downloads/sa-mp-0.3.7-R4-install.exe
