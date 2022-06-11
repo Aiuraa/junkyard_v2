5e84c1572e5ef82776ed61d7bd39663f1684bb68
Aiura <104730381+Aiuraa@users.noreply.github.com>
2022-06-11 07:09:51 +0000

    Create omp.sh

---
+++ samp/omp.sh
@@ -0,0 +1,1 @@
+sudo wget https://github.com/openmultiplayer/server-beta/releases/download/build5/open.mp-linux.tar.gz && sudo tar xzf open.mp-linux.tar.gz && rm -f open.mp-linux.tar.gz && sudo chmod 700 *
